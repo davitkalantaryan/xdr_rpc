e95177b514db87f2edf9a68111bfaa7eb3068dcd
Davit Kalantaryan <davit.kalantar@desy.de>
2020-03-19 08:53:24 +0100

    Modified compilation script to include there other platforms
    [why]
    In order to compile for arm,arm64,x86
    
    [how]
    By providing argument arm, will compile for arm.
    One can also provide argument release, then release build will take place
    Or one can provide arg rebuild, then rebuild instead of build will tae place

--- build_lib.bat
+++ build_lib.bat
@@ -9,16 +9,50 @@
 ::
 
 @ECHO off
-SETLOCAL enableextensions
+SETLOCAL EnableDelayedExpansion
 
-:: here we define platformtarhet to build, platforms are following
+:: here we define default platformtarget to build, platforms are following
+:: other platforms should be provided as first argument
 :: ARM, ARM64, x86, x64
 set PlatformTarget=x64
 set Configuration=Debug
+set ActionConfirm=Build
 
 SET  scriptDirectory=%~dp0
 set  currentDirectory=%cd%
 
+:: handling arguments
+::set argC=0
+::for %%x in (%*) do Set /A argC+=1
+set nextArg=%0
+for %%x in (%*) do (
+	set nextArg=%%x
+	call :parse_argument
+)
+
+echo action=%ActionConfirm%,platform=%PlatformTarget%,configuration=%Configuration%
+::exit /b 0
+
 msbuild %scriptDirectory%prj\core\doocs_win_xdr_rpc_vs\doocs_win_xdr_rpc.sln /t:Build /p:Configuration=%Configuration% /p:Platform=%PlatformTarget%
+
+:parse_argument
+	set isNextArgPlatform=true
+	if /i not "!nextArg!"=="ARM" if /i not "!nextArg!"=="ARM64" if /i not "!nextArg!"=="x86" if /i not "!nextArg!"=="x64" (set isNextArgPlatform=false)
+	if "!isNextArgPlatform!"=="true" (set PlatformTarget=!nextArg!) else (
+		set isNextArgAction=true
+		if /i not "!nextArg!"=="Build" if /i not "!nextArg!"=="Rebuild" if /i not "!nextArg!"=="Clean" (set isNextArgAction=false)
+		if "!isNextArgAction!"=="true" (set ActionConfirm=!nextArg!) else (
+			set isNextArgConfiguration=true
+			if /i not "!nextArg!"=="Debug" if /i not "!nextArg!"=="Release" (set isNextArgConfiguration=false)
+			if "!isNextArgConfiguration!"=="true" (set Configuration=!nextArg!) else (
+				echo Unknown argument !nextArg!.
+				echo Valid platforms are ARM, ARM64, x86 and x64. Using defalt x64.
+				echo Valid configurations are Debug and Release. Using default Debug.
+				echo Valid actions are Build, Rebuild and Clen. Using default Build.
+			)
+		)		
+	)
+
+	exit /b 0
 
 ENDLOCAL
