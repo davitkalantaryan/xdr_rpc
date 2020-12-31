be3796a9a0a50747d848ff27779a0d8b87a5e4ce
Davit Kalantaryan <davit.kalantar@desy.de>
2021-01-01 00:27:39 +0100

    added sources for function necessary for serverlib. Sources are src/core/for_server directory

---
+++ include/rpc/svc_soc.h
@@ -0,0 +1,11 @@
+
+#ifndef RPC_SVC_SOC_H
+#define RPC_SVC_SOC_H
+
+#include <rpc/wrpc_first_com_include.h>
+#include <rpc/types.h>
+
+MINI_XDR_BEGIN_C_DECLS
+MINI_XDR_END_C_DECLS
+
+#endif  // #ifndef RPC_SVC_SOC_H
