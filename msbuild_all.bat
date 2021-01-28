6b0659b8d9dbe6d2190c48ce42048d21c4241f29
Davit Kalantaryan <davit.kalantar@desy.de>
2021-01-28 12:28:46 +0100

    modified library compilation sctript to compile for several platforms|configurations at once

--- msbuild_all.bat
+++ msbuild_all.bat
@@ -1,6 +1,6 @@
 ::
-:: File:			build_lib.bat
-:: Created on:		2020 Feb 27
+:: File:			msbuild_all.bat
+:: Created on:		2020 Jul 24
 :: Autor:			Davit Kalantaryan (davit.kalantaryan@desy.de)
 ::
 :: Purpose:	
@@ -8,19 +8,23 @@
 :: Argumets: 
 ::
 
-@ECHO off
-SETLOCAL EnableDelayedExpansion
+@echo off
+setlocal EnableDelayedExpansion
 
 :: here we define default platformtarget to build, platforms are following
 :: other platforms should be provided as first argument
 :: ARM, ARM64, x86, x64
-set PlatformTarget=x64
-set Configuration=Debug
+set PlatformTarget=ARM,ARM64,x86,x64
+set Configuration=Debug,Release
 set ActionConfirm=Build
 
 set scriptName=%0
-SET scriptDirectory=%~dp0
+set scriptDirectory=%~dp0
 set currentDirectory=%cd%
+rem cd %scriptDirectory%..
+cd %scriptDirectory%
+set repositoryRoot=%cd%\
+
 
 :: handling arguments
 ::set argC=0
@@ -32,23 +36,42 @@ for %%x in (%*) do (
 		exit /b 0
 	)
 	set nextArg=%%x
+	set possibleQuote=!nextArg:~0,1!
+	set possibleQuote|find """" >nul && set quote_found=yes||set quote_found=no
+	if "!quote_found:~0,3!"=="yes" ( 
+		set nextArg=!nextArg:~1,-1! 
+		echo fixed arg: !nextArg!
+	)
 	call :parse_argument
 	if not "!ERRORLEVEL!"=="0" (exit /b %ERRORLEVEL%)
 )
 
-echo action=%ActionConfirm%,platform=%PlatformTarget%,configuration=%Configuration%
-msbuild %scriptDirectory%prj\core\xdr_rpc_vs\xdr_rpc.sln /t:!ActionConfirm! /p:Configuration=!Configuration! /p:Platform=!PlatformTarget!
-exit /b 0
+echo action=%ActionConfirm%,PlatformTarget=!PlatformTarget!,configuration=%Configuration%
+
+
+for %%p in (%PlatformTarget%) do (
+	echo "!!!!!!!!!!!! platform %%p"
+	for %%c in (%Configuration%) do (
+		echo "!!!!!!!!!!!! !!!!!!!!!!!! compiling for configuration %%c"
+		call msbuild "%repositoryRoot%workspaces\xdr_rpc_vs\xdr_rpc.sln" /t:!ActionConfirm! /p:Configuration=%%c /p:Platform=%%p
+		if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
+	)
+)
+
+
+exit /b %ERRORLEVEL%
+
 
 :parse_argument
 	set isNextArgPlatform=true
-	if /i not "!nextArg!"=="ARM" if /i not "!nextArg!"=="ARM64" if /i not "!nextArg!"=="x86" if /i not "!nextArg!"=="x64" (set isNextArgPlatform=false)
+	if /i not "!nextArg:~0,3!"=="ARM" if /i not "!nextArg:0,5!"=="ARM64" if /i not "!nextArg:~0,3!"=="x86" if /i not "!nextArg:~0,3!"=="x64" (set isNextArgPlatform=false)
 	if "!isNextArgPlatform!"=="true" (set PlatformTarget=!nextArg!) else (
 		set isNextArgAction=true
 		if /i not "!nextArg!"=="Build" if /i not "!nextArg!"=="Rebuild" if /i not "!nextArg!"=="Clean" (set isNextArgAction=false)
 		if "!isNextArgAction!"=="true" (set ActionConfirm=!nextArg!) else (
 			set isNextArgConfiguration=true
-			if /i not "!nextArg!"=="Debug" if /i not "!nextArg!"=="Release" (set isNextArgConfiguration=false)
+			rem if /i not "!nextArg!"=="Debug" if /i not "!nextArg!"=="Release" (set isNextArgConfiguration=false)
+			if /i not "!nextArg:~0,5!"=="Debug" if /i not "!nextArg:~0,7!"=="Release" (set isNextArgConfiguration=false)
 			if "!isNextArgConfiguration!"=="true" (set Configuration=!nextArg!) else (
 				echo Unknown argument !nextArg!.
 				call :call_help
@@ -66,4 +89,4 @@ exit /b 0
 	echo Call !scriptName! help to displaye help message
 	exit /b 0
 
-ENDLOCAL
+endlocal
