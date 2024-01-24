5a8ac243b53e789af6dcc4dc797497061e69af22
Davit Kalantaryan <davit.kalantar@desy.de>
2024-01-24 10:38:48 +0100

    src/core/include/xdr_rpc_priv_lists.h file was added

---
+++ src/core/include/xdr_rpc_priv_lists.h
@@ -0,0 +1,29 @@
+//
+// file:			xdr_rpc_priv_lists.h
+// path:			src/core/include/xdr_rpc_priv_lists.h
+// created on:		2024 Jan 24
+//
+
+#ifndef SRC_CORE_INCLUDE_XDR_RPC_PRIV_LISTS_H
+#define SRC_CORE_INCLUDE_XDR_RPC_PRIV_LISTS_H
+
+
+#include <rpc/wrpc_first_com_include.h>
+#include <rpc/svc.h>
+
+
+MINI_XDR_BEGIN_C_DECLS
+
+
+//MINI_XDR_DLL_PRIVATE extern int g_nLogLevel;
+struct SVCXPRTPrivListItem {
+	struct SVCXPRTPrivListItem* prev, * next;
+	SVCXPRT* xprt;
+};
+
+
+
+MINI_XDR_END_C_DECLS
+
+
+#endif  // #ifndef SRC_CORE_INCLUDE_XDR_RPC_PRIV_LISTS_H
